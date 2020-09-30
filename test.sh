47b8db631cd55d2d6efc161b8eb16d4d48797944
Mike McCann <mccann@mbari.org>
2020-09-30 14:26:42 -0700

    Add --clobber --noinput for running at command line

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python logs2netcdfs.py --help
 python calibrate_align.py --help
-python logs2netcdfs.py --auv_name Dorado389 --mission 2020.245.00 --portal http://stoqs.mbari.org:8080/auvdata/v1 -v
+python logs2netcdfs.py --auv_name Dorado389 --mission 2020.245.00 --portal http://stoqs.mbari.org:8080/auvdata/v1 --clobber --noinput -v
 pytest
 exit $?
