a8a66d66537169a233ec25a134df6c4f37fb6167
Mike McCann <mccann@mbari.org>
2020-10-02 16:03:00 -0700

    Factor out fixtures into pytest's conftest.py

--- test_hs2_proc.py
+++ test_hs2_proc.py
@@ -1,35 +1,8 @@
 import logging
 import numpy as np
-import os
 import pytest
 from calibrate_align import CalAligned_NetCDF
-from argparse import Namespace
-from hs2_proc import (hs2_read_cal_file, typ_absorption, purewater_scatter, 
-                      _get_gains, hs2_calc_bb)
-from logs2netcdfs import MISSIONLOGS
-
-@pytest.fixture(scope='session', autouse=True)
-def mission_data():
-    '''Load a short recent mission to have some real data to work with
-    '''
-    cal_netcdf = CalAligned_NetCDF()
-    ns = Namespace()
-    ns.base_path = 'auv_data'
-    ns.auv_name = 'dorado389'
-    ns.mission = '2020.245.00'
-    ns.plot = None
-    cal_netcdf.args = ns
-    ##cal_netcdf.logger.setLevel(logging.ERROR)
-    cal_netcdf.process_logs()
-    return cal_netcdf
-
-@pytest.fixture(scope='session', autouse=True)
-def calibration(mission_data):
-    md = mission_data
-    logs_dir = os.path.join(md.args.base_path, md.args.auv_name, 
-                            MISSIONLOGS, md.args.mission)
-    cal_fn = os.path.join(logs_dir, md.sinfo['hs2']['cal_filename'])
-    return hs2_read_cal_file(cal_fn)
+from hs2_proc import typ_absorption, purewater_scatter, _get_gains, hs2_calc_bb
 
 def test_typ_absorption():
     assert round(typ_absorption(420), 4) == 0.0235
