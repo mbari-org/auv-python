1c893e06ae16ab2c5c88f947732484ea4b481127
Mike McCann <mccann@mbari.org>
2020-10-05 14:12:32 -0700

    Simplify to just run the tests

--- test.sh
+++ test.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-python logs2netcdfs.py --help
-python calibrate_align.py --help
 python logs2netcdfs.py --auv_name Dorado389 --mission 2020.245.00 --portal http://stoqs.mbari.org:8080/auvdata/v1 --clobber --noinput -v
-python calibrate_align.py --auv_name Dorado389 --mission 2020.245.00 -v 2
-pytest --debug
+pytest
 exit $?
