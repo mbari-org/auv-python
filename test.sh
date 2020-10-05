00eb3d90dcf8016c8f83ae5129b991bf57541cef
Mike McCann <mccann@mbari.org>
2020-10-05 14:42:20 -0700

    Add echo statements to serve as comments

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "Download and process a mission to use for testing..."
 python logs2netcdfs.py --auv_name Dorado389 --mission 2020.245.00 --portal http://stoqs.mbari.org:8080/auvdata/v1 --clobber --noinput -v
+echo "Run tests..."
 pytest
 exit $?
