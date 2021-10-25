295c0229aa33a5d3044516977624d75525831d41
Mike McCann <mccann@mbari.org>
2021-10-25 14:43:51 -0700

    Created directory structure from https://drivendata.github.io/cookiecutter-data-science/

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Python code for processing MBARI Dorado-class AUV instrument data',
+    author='Mike McCann',
+    license='BSD-3',
+)
