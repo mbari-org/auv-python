1dd4c98cfe49c26c6df328fbfe483c573db8c487
Mike McCann <mccann@mbari.org>
2021-10-25 17:13:50 -0700

    Use ../../data dir provided by ccds. Use 'python -m pytest' so that modules are found in the venv

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Download and process a mission to use for testing..."
-python logs2netcdfs.py --auv_name Dorado389 --mission 2020.245.00 --portal http://stoqs.mbari.org:8080/auvdata/v1 --clobber --noinput -v
+cd src/data
+python logs2netcdfs.py --auv_name Dorado389 --mission 2020.245.00 --portal http://stoqs.mbari.org:8080/auvdata/v1 \
+                       --base_path ../../data/auv_data --clobber --noinput -v
 echo "Run tests..."
-pytest
+python -m pytest 
 exit $?
