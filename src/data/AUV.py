699d4bf37e12df0e6c0b3427c97da8ac24b34b28
Mike McCann <mccann@mbari.org>
2021-11-19 08:54:04 -0800

    Format with black

--- src/data/AUV.py
+++ src/data/AUV.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-'''
+"""
 Base classes for reading and writing data for MBARI's Dorado class AUVs.
 
 --
 Mike McCann
 MBARI 30 March 2020
-'''
+"""
 
 import os
 import sys
@@ -24,33 +24,37 @@ from subprocess import Popen, PIPE
 
 
 class AUV(object):
-    '''
-    Container for common methods to be reused by BEDs processing software.
-    Initially used by bed2netcdf.py and bed2x3d.py to read the data.
-    '''
-
     def add_global_metadata(self):
-        '''Use instance variables to write metadata specific for the data that are written
-        '''
 
-        iso_now = datetime.utcnow().isoformat() + 'Z'
+        iso_now = datetime.utcnow().isoformat() + "Z"
 
-        self.nc_file.netcdf_version = '4'
-        self.nc_file.Conventions = 'CF-1.6'
+        self.nc_file.netcdf_version = "4"
+        self.nc_file.Conventions = "CF-1.6"
         self.nc_file.date_created = iso_now
         self.nc_file.date_update = iso_now
         self.nc_file.date_modified = iso_now
-        self.nc_file.featureType = 'trajectory'
+        self.nc_file.featureType = "trajectory"
 
-        self.nc_file.comment = ('Autonomous Underwater Vehicle data....'
-                               'https://bitbucket.org/mbari/auv-python')
+        self.nc_file.comment = (
+            "Autonomous Underwater Vehicle data...."
+            "https://bitbucket.org/mbari/auv-python"
+        )
 
-        self.nc_file.time_coverage_start = coards.from_udunits(self.time[0], self.time.units).isoformat() + 'Z'
-        self.nc_file.time_coverage_end = coards.from_udunits(self.time[-1], self.time.units).isoformat() + 'Z'
+        self.nc_file.time_coverage_start = (
+            coards.from_udunits(self.time[0], self.time.units).isoformat() + "Z"
+        )
+        self.nc_file.time_coverage_end = (
+            coards.from_udunits(self.time[-1], self.time.units).isoformat() + "Z"
+        )
 
-        self.nc_file.distribution_statement = 'Any use requires prior approval from MBARI'
+        self.nc_file.distribution_statement = (
+            "Any use requires prior approval from MBARI"
+        )
         self.nc_file.license = self.nc_file.distribution_statement
-        self.nc_file.useconst = 'Not intended for legal use. Data may contain inaccuracies.'
-        self.nc_file.history = 'Created by "%s" on %s' % (' '.join(sys.argv), iso_now,)
-
-
+        self.nc_file.useconst = (
+            "Not intended for legal use. Data may contain inaccuracies."
+        )
+        self.nc_file.history = 'Created by "%s" on %s' % (
+            " ".join(sys.argv),
+            iso_now,
+        )
