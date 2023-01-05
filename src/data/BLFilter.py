c30f72fdfef6276c642ba1dbf5e879958468d3aa
Mike McCann <mccann@mbari.org>
2023-01-05 13:58:59 -0800

    Add (commented out) np.roll() fixes

--- src/data/BLFilter.py
+++ src/data/BLFilter.py
@@ -5,13 +5,13 @@ __credits__ = ["MBARI"]
 __license__ = "GPL-3.0"
 __maintainer__ = "Ben Raanan"
 __email__ = "byraanan at mbari.org"
-__doc__ = '''
+__doc__ = """
 
 Defines the Filter class for filtering UBAT data using rolling window filters.
 
 @author: __author__
 @license: __license__
-'''
+"""
 import logging
 
 import numpy as np
@@ -21,12 +21,10 @@ logger = logging.getLogger("pybl")
 
 
 class Filter:
-
     def __init__(self, window_size, target_record_size=60):
 
         self.window_size = window_size  # rolling filter window size
         self.target_record_size = target_record_size  # target size of filtered record
-
 
     def apply_filter(self, data: tuple, filt_func):
         """
@@ -42,14 +40,19 @@ class Filter:
         # apply filter and extract filtered data relevant to the current record
         background_ = self.filter(data_, filt_func)
 
+        # shift the filtered data to the left by half the window size
+        # background_ = np.roll(background_, int(-self.window_size / 2))
+
         # append the filtered data form the current record
         data_filt_ = np.concatenate([data_filt_, background_])
+
+        # shift the filtered data to the left by half the window size
+        # data_filt_ = np.roll(data_filt_, int(-self.window_size / 2))
 
         # apply mean filter and extract filtered data relevant to the current record
         background_smooth_ = self.filter(data_filt_, Mean)
 
         return background_, background_smooth_
-
 
     def filter(self, data, filt_func):
         """
@@ -63,14 +66,19 @@ class Filter:
 
         # determine appropriate window type: default to a fixed size window, but use a variable
         # window if sample size is insufficient
-        window_type = 'fixed' if (N - self.target_record_size >= self.window_size - 1) else 'variable'
+        window_type = (
+            "fixed"
+            if (N - self.target_record_size >= self.window_size - 1)
+            else "variable"
+        )
         # logger.info('Applying a {} {} dp {} filter to data with {} dp.'.format(window_type, self.window_size,
         #                                                                        filt_func.__name__, len(data)))
         # apply the filter!
-        filt_ = list(filt_func(data, window_size=self.window_size, window_type=window_type))
+        filt_ = list(
+            filt_func(data, window_size=self.window_size, window_type=window_type)
+        )
         # extract the filtered data that's relevant to the current record and return
         return self.extract_filtered_data(filt_, window_type)
-
 
     def extract_filtered_data(self, filt_data, window_type):
         """
@@ -85,9 +93,9 @@ class Filter:
         N = len(filt_data)  # actual size of filtered record
         win_size = self.window_size
 
-        if window_type == 'fixed' or (window_type == 'variable' and N < win_size):
+        if window_type == "fixed" or (window_type == "variable" and N < win_size):
             ret_val = filt_data[-n:]
         else:
-            ret_val = filt_data[1-n-win_size:1-win_size]
+            ret_val = filt_data[1 - n - win_size : 1 - win_size]
 
         return np.array(ret_val)
