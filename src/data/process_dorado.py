7fbe5111078160e39b6f7b9c93b7d099a369fbc9
Mike McCann <MBARIMike@gmail.com>
2023-11-17 10:42:54 -0800

    Add CALIBRATION_DIR

--- src/data/process_dorado.py
+++ src/data/process_dorado.py
@@ -32,9 +32,10 @@ class DoradoProcessor(Processor):
 if __name__ == "__main__":
     VEHICLE = "dorado"
     VEHICLE_DIR = "/Volumes/AUVCTD/missionlogs"
+    CALIBRATION_DIR = "/Volumes/DMO/MDUC_CORE_CTD_200103/Calibration Files"
     MOUNT_DIR = "cifs://atlas.shore.mbari.org/AUVCTD"
     START_YEAR = 2003
 
-    proc = DoradoProcessor(VEHICLE, VEHICLE_DIR, MOUNT_DIR)
+    proc = DoradoProcessor(VEHICLE, VEHICLE_DIR, MOUNT_DIR, CALIBRATION_DIR)
     proc.process_command_line()
     proc.process_missions(START_YEAR)
