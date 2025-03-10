387ae0320e51bedbcb681eeddc0d43237d1e8c59
Mike McCann <MBARIMike@gmail.com>
2025-03-10 09:16:05 -0700

    Remove proposed test for LOPC processing.
    This short lopc data containing mission does not expose the regression that is fixed by commit fbbff71987d7cf72153342f35742d35a844c79ea

--- src/data/conftest.py
+++ src/data/conftest.py
@@ -29,12 +29,6 @@ TEST_I2MAP_CALIBRATION_DIR = Path(
 TEST_I2MAP_MOUNT_DIR = ""
 TEST_I2MAP_START_YEAR = 2018
 
-# Use a short mission for testing: dorado 2010.341.00 http://stoqs.mbari.org/p/SVGDeLc
-# The lopc_dorado_processing fixture will process the mission, all other TEST_ variables
-# are the same as for Dorado389 2011.256.02
-LOPC_MISSION = "2010.341.00"  # http://stoqs.mbari.org/p/SVGDeLc
-LOPC_START_YEAR = 2010
-
 
 @pytest.fixture(scope="session", autouse=False)
 def mission_data():
@@ -147,42 +141,3 @@ def complete_i2map_processing():
     proc.args = ns
     proc.process_missions(TEST_START_YEAR)
     return proc
-
-
-@pytest.fixture(scope="session", autouse=False)
-def lopc_dorado_processing():
-    """Load a short mission with LOPC data for testing of lopc processing"""
-    proc = Processor(TEST_VEHICLE, TEST_VEHICLE_DIR, TEST_MOUNT_DIR, TEST_CALIBRATION_DIR)
-    ns = Namespace()
-    ns.base_path = os.getenv("BASE_PATH", BASE_PATH)
-    ns.auv_name = TEST_VEHICLE
-    ns.mission = LOPC_MISSION
-    ns.start_year = LOPC_START_YEAR
-    # There are several options that need to be set to run the full processing
-    ns.clobber = False
-    proc.commandline = "args set in conftest.py::lopc_dorado_processing()"
-    ns.local = True
-    ns.noinput = True
-    ns.noreprocess = False
-    ns.use_portal = False
-    ns.freq = FREQ
-    ns.mf_width = MF_WIDTH
-    ns.flash_threshold = FLASH_THRESHOLD
-    # Set step flags to false to force all steps to run as the logic in
-    # process_mission() is not fully implemented.
-    ns.download_process = False
-    ns.calibrate = False
-    ns.align = False
-    ns.resample = False
-    ns.create_products = False
-    ns.archive = False
-    ns.archive_only_products = False
-    ns.email_to = None
-    ns.cleanup = False
-    ns.no_cleanup = True
-    ns.skip_download_process = False
-    ns.last_n_days = 0
-    ns.num_cores = 1
-    ns.verbose = 1
-    proc.args = ns
-    proc.process_missions(TEST_START_YEAR)
