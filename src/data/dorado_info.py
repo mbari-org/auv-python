449206f6b0304cadc79335c2d35073f066953d45
Mike McCann <MBARIMike@gmail.com>
2025-05-20 17:25:27 -0700

    Add mission 2025.139.04.

--- src/data/dorado_info.py
+++ src/data/dorado_info.py
@@ -2914,3 +2914,11 @@ dorado_info["2025.098.01"] = {
         " - ctdToUse = ctd1 "
     ),
 }
+dorado_info["2025.139.04"] = {
+    "program": f"{MBTSLINE}",
+    "comment": (
+        "Monterey Bay MBTS Mission - 13985G"
+        " Biolume, ISUS, and LISST payloads removed "
+        " - ctdToUse = ctd1 "
+    ),
+}
