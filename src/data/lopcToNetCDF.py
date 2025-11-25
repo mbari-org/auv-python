95133f75a01f5ac7a758f11904684cb145de1571
Mike McCann <MBARIMike@gmail.com>
2025-11-25 14:33:57 -0800

    Remove ending period from summary text.

--- src/data/lopcToNetCDF.py
+++ src/data/lopcToNetCDF.py
@@ -1895,7 +1895,7 @@ class LOPC_Processor:
             " produced by the LOPC instrument.  The data in this file are to be"
             " considered as simple time series data only and are as close to the"
             " original data as possible.  Further processing is required to turn"
-            " the data into a time series of profiles."
+            " the data into a time series of profiles"
         )
         self.ncFile.keywords = "plankton, particles, detritus, marine snow, particle counter"
         self.ncFile.Conventions = "CF-1.6"
